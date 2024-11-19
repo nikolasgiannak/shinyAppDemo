38794e70520bb0b9414de5103d6b642b01fd4dcd
Nikolas Giannakis <ngiannak@exseed.ed.ac.uk>
2024-11-19 14:48:02 +0000

    code for exercise 1.6 but with 3 instead of two numbers

--- shinyAppDemo/app.R
+++ shinyAppDemo/app.R
@@ -53,23 +53,35 @@ ui <- fluidPage(titlePanel("Basic Demo"),
 #}
 
 # 2nd version using a render fuction paired with an outpiut function
-server <- function(input, output){
-  #count the choices/favourite things through renderText function
-  observeEvent(input$count_favourite_things,#create a new column at input named count_things
-               {
-                 n = length(input$fav_things)
-                 countText = sprintf("You have %d fav things", n)
-    output$n_fav_things = renderText(countText)
-  })
-  
-}
+#server <- function(input, output){
+#  #count the choices/favourite things through renderText function
+#  observeEvent(input$count_favourite_things,#create a new column at input named count_things
+#               {
+#                 n = length(input$fav_things)
+#                 countText = sprintf("You have %d fav things", n)
+#    output$n_fav_things = renderText(countText)
+# })
+#  
+# }
 
 # 3rd option to do the same thing by using reactive() to create a new function called count_text, which updates 
 # the value when the input for the reactive function changes
 
 # Here isolate is used to prevent count_text() from changing when a user clicks the checkboxes
 
-
+server <- function(input, output) {
+  # update count_text on fav_things
+  count_text <- reactive({
+    input$count_favourite_things # just here to trigger the reactive
+    fav_things <-
+      isolate(input$fav_things) # don't trigger on checks
+    n <- length(fav_things)
+    sprintf("You have %d favourite things", n)
+  })
+  
+  # display count_text when it updates
+  output$n_fav_things <- renderText(count_text())
+}
 
 
 
