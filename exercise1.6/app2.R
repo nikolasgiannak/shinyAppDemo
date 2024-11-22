d74001a2bcfa327805ea387bf34783e449a62b64
Nikolas Giannakis <ngiannak@exseed.ed.ac.uk>
2024-11-22 10:29:26 +0000

    aadd a 4th number

--- exercise1.6/app2.R
+++ exercise1.6/app2.R
@@ -15,14 +15,15 @@ ui <- fluidPage(titlePanel("Addition Demo"),
                   sidebarPanel(
                     numericInput("n1", "Write the first number of choice", 0, min =0, max =100),
                     numericInput("n2", "Write the second number of choice", 0, min = 0, max = 50),
-                    numericInput("n3", "Write the third number of choice", 0, min = 0, max = 100 )
+                    numericInput("n3", "Write the third number of choice", 0, min = 0, max = 100 ),
+                    numericInput("num4", "Write the 4th number of choice", 0, min = 0, max = 100 )
                       ),
                   actionButton(
                     inputId = "addition",
                     label = "Add numbers",
                                   )
                   ),
-                  mainPanel(textOutput(outputId = "n1_n2_n3"))
+                  mainPanel(textOutput(outputId = "n1_n2_n3_num4"))
                 )
 
 # Define server logic required to draw a histogram
@@ -31,8 +32,8 @@ server <- function(input, output)
   # perfrom the addition of numbers
    observeEvent(input$addition, {
                 sum <- input$n1 + input$n2 + input$n3
-                add_text <- sprintf("%d + %d + %d = %d", input$n1, input$n2, input$n3, sum)
-                output$n1_n2_n3 <- renderText(add_text)
+                add_text <- sprintf("%d + %d + %d + %d = %d", input$n1, input$n2, input$n3,input$num4, sum)
+                output$n1_n2_n3_num4 <- renderText(add_text)
    
    }
    )
