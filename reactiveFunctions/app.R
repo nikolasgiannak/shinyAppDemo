2bda3dcfde951129e72cd6e584c84d0edc4169d7
Nikolas Giannakis <ngiannak@exseed.ed.ac.uk>
2024-11-29 13:38:04 +0000

    update details for color in ggplot geom_point and geom_smooth

---
+++ reactiveFunctions/app.R
@@ -0,0 +1,83 @@
+
+
+# shinyintro::clone("reactive_demo", "myapp")
+
+# libraries ----
+suppressPackageStartupMessages({
+  library(shiny)
+  library(shinyjs)
+  library(shinydashboard)
+  library(ggplot2)
+  library(dplyr)
+})
+
+# setup ----
+
+
+# functions ----
+source("scripts/func.R") # helper functions
+
+# user interface ----
+
+
+## UI ----
+ui <- dashboardPage(
+  skin = "purple",
+  dashboardHeader(title = "Reactive Demo",
+                  titleWidth = "100%"),
+  dashboardSidebar(disable = TRUE),
+  dashboardBody(
+    shinyjs::useShinyjs(),
+    tags$head(
+      # links to files in www/
+      tags$link(rel = "stylesheet", type = "text/css", href = "basic_template.css"),
+      tags$link(rel = "stylesheet", type = "text/css", href = "custom.css"),
+      tags$script(src = "custom.js")
+    ),
+    box(width = 3,
+        title = "Diamonds",
+        solidHeader = TRUE,
+        selectInput("cut", "Cut", levels(diamonds$cut)),
+        selectInput("color", "Color", levels(diamonds$color)),
+        selectInput("clarity", "Clarity", levels(diamonds$clarity)),
+        actionButton("update", "Update Plot")
+    ),
+    box(width = 9,
+        title = "Plot",
+        solidHeader = TRUE,
+        textOutput("title"),
+        plotOutput("plot")
+    )
+  )
+)
+
+
+# server ----
+server <- function(input, output, session) {
+  output$plot <- renderPlot({
+    data <- filter(diamonds,
+                   cut == input$cut,
+                   color == input$color,
+                   clarity == input$clarity)
+    
+    ggplot(data, aes(carat, price)) +
+      geom_point(color = "red", alpha = 0.5) +
+      geom_smooth(method = lm, color = "#605CA8")
+  })
+  
+  output$title <- renderText({
+    input$update # just here to trigger the function
+    
+    sprintf("Cut: %s, Color: %s, Clarity: %s",
+            input$cut,
+            input$color,
+            input$clarity)
+  })
+} 
+
+shinyApp(ui, server)
+
+
+
+
+
